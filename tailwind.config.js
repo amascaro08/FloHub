29d8ba5558f0fe6c00d39ea89abf86e315b3901f
Alvii <33187195+amascaro08@users.noreply.github.com>
2025-04-25 06:33:06 +0000

    rweak

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  safelist: [
-    'react-resizable',
-    'react-resizable-handle',
-    'react-resizable-handle-se',
-    'react-resizable-handle-s',
-    'react-resizable-handle-e',
-  ],
+
   theme: {
     extend: {
       colors: {
