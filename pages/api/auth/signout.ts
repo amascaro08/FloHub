7ff7e0d9a4d4b354e29382122deb64a04332b355
Alvii <33187195+amascaro08@users.noreply.github.com>
2025-04-28 01:25:44 +0000

    sign out

--- pages/api/auth/signout.ts
+++
@@ -1,7 +0,0 @@
-import { signOut } from "next-auth/react";
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  await signOut({ redirect: false });
-  res.status(200).json({ message: "Signed out successfully" });
-}
