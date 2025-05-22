b11131b0da6a37f9f3e19ddef195725909a0d9d4
Alvii <33187195+amascaro08@users.noreply.github.com>
2025-05-22 03:07:18 +0000

    fixes

--- types/calendar.ts
+++
@@ -1,4 +0,0 @@
-// This file should ideally not exist if using a .d.ts for types.
-// Reverting to an empty state or removing it might be necessary
-// depending on the project's build configuration.
-// For now, I will make it an empty file to remove the errors.
